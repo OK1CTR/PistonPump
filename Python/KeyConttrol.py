7d472832519c3f38e8e09bc9914617814076fe2d
Richard Linhart <OK1CTR@gmail.com>
2025-01-25 20:58:23 +0100

    Change Changed keys and removed prints in KeyControl.py.

--- Python/KeyConttrol.py
+++ Python/KeyConttrol.py
@@ -7,45 +7,50 @@ import keyboard
 if __name__ == '__main__':
 
     # Create Modbus client
-    client = ModbusClient.ModbusSerialClient(port="COM3", stopbits=1, bytesize=8, parity='N', baudrate=115200)
+    client = ModbusClient.ModbusSerialClient(port="COM5", stopbits=1, bytesize=8, parity='N', baudrate=115200)
     if not client.connect():
         exit('Connection failed')
 
     while True:
         a = keyboard.read_key()
-        if a == '\\':  # Send wave
+        if a == 't':  # Send wave
             result_ = client.write_registers(address=0, values=[CMD_WAVE], slave=modbus_slave_addr)
-            print(f'Write Holding Regs: {result_}')
+            # print(f'Write Holding Regs: {result_}')
             continue
 
-        elif a == 'c':  # pulse forward
+        elif a == 'p':  # pulse forward
             result_ = client.write_registers(address=0, values=[CMD_STEP_FORWARD], slave=modbus_slave_addr)
-            print(f'Write Holding Regs: {result_}')
+            # print(f'Write Holding Regs: {result_}')
             continue
 
-        elif a =='x':  # pulse back
+        elif a =='o':  # pulse back
             result_ = client.write_registers(address=0, values=[CMD_STEP_REWIND], slave=modbus_slave_addr)
-            print(f'Write Holding Regs: {result_}')
+            # print(f'Write Holding Regs: {result_}')
             continue
 
-        elif a =='0':  # emergency stop
+        elif a =='b':  # emergency stop
             result_ = client.write_registers(address=0, values=[CMD_STOP], slave=modbus_slave_addr)
-            print(f'Write Holding Regs: {result_}')
+            # print(f'Write Holding Regs: {result_}')
             continue
 
-        elif a =='1':  # emergency stop
+        elif a =='q':  # emergency stop
             result_ = client.write_registers(address=1, values=[1000, 250, 1000, 250], slave=modbus_slave_addr)
-            print(f'Write Holding Regs: {result_}')
+            # print(f'Write Holding Regs: {result_}')
             continue
 
-        elif a =='2':  # emergency stop
+        elif a =='w':  # emergency stop
             result_ = client.write_registers(address=1, values=[1000, 500, 1000, 500], slave=modbus_slave_addr)
-            print(f'Write Holding Regs: {result_}')
+            # print(f'Write Holding Regs: {result_}')
             continue
 
-        elif a =='3':  # emergency stop
+        elif a =='e':  # emergency stop
             result_ = client.write_registers(address=1, values=[1000, 1000, 1000, 1000], slave=modbus_slave_addr)
-            print(f'Write Holding Regs: {result_}')
+            # print(f'Write Holding Regs: {result_}')
+            continue
+
+        elif a =='r':  # repeat
+            result_ = client.write_registers(address=0, values=[CMD_REPEAT], slave=modbus_slave_addr)
+            # print(f'Write Holding Regs: {result_}')
             continue
 
         elif a == 'q':  # Quit
